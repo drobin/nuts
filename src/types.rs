e4d88751a727d0acbee24c13333f3707521423be
Robin Doer <robin@robind.de>
2020-06-16 10:56:49 +0200

    Initial commit
    Commit the Cipher enum

---
+++ src/types.rs
@@ -0,0 +1,59 @@
+// MIT License
+//
+// Copyright (c) 2020 Robin Doer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#[cfg(test)]
+mod tests;
+
+/// Supported cipher algorithms.
+pub enum Cipher {
+    /// No encryption
+    None,
+
+    /// AES with a 128-bit key in CTR mode
+    Aes128Ctr,
+}
+
+impl Cipher {
+    /// Returns the key size of the cipher.
+    pub fn key_size(&self) -> u32 {
+        match self {
+            Cipher::None => 0,
+            Cipher::Aes128Ctr => 16,
+        }
+    }
+
+    /// Returns the IV size of the cipher.
+    pub fn iv_size(&self) -> u32 {
+        match self {
+            Cipher::None => 0,
+            Cipher::Aes128Ctr => 16,
+        }
+    }
+
+    /// Returns the block size of the cipher.
+    pub fn block_size(&self) -> u32 {
+        match self {
+            Cipher::None => 1,
+            Cipher::Aes128Ctr => 1,
+        }
+    }
+}
---
+++ src/types/tests.rs
@@ -0,0 +1,53 @@
+// MIT License
+//
+// Copyright (c) 2020 Robin Doer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+use crate::types::Cipher;
+
+#[test]
+fn cipher_key_size_none() {
+    assert_eq!(Cipher::None.key_size(), 0);
+}
+
+#[test]
+fn cipher_key_size_aes128_ctr() {
+    assert_eq!(Cipher::Aes128Ctr.key_size(), 16);
+}
+
+#[test]
+fn cipher_iv_size_none() {
+    assert_eq!(Cipher::None.iv_size(), 0);
+}
+
+#[test]
+fn cipher_iv_size_aes128_ctr() {
+    assert_eq!(Cipher::Aes128Ctr.iv_size(), 16);
+}
+
+#[test]
+fn cipher_block_size_none() {
+    assert_eq!(Cipher::None.block_size(), 1);
+}
+
+#[test]
+fn cipher_block_size_aes128_ctr() {
+    assert_eq!(Cipher::Aes128Ctr.block_size(), 1);
+}
