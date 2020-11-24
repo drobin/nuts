0806d72e0dd4639cb970fdcf979fce7b1e5f7a5b
Robin Doer <robin@robind.de>
2020-11-24 07:49:55 +0100

    Commit some initial crate docs

--- nuts/src/lib.rs
+++ nuts/src/lib.rs
@@ -20,6 +20,121 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+//! A secure storage library.
+//!
+//! The _nuts_ library implements a secure storage library, where data are
+//! stored in a container. The container is divide into blocks, which are
+//! encrypted separately.
+//!
+//! The container configuration contains all information needed to open the
+//! container. It is located in the first block of the container. It has a
+//! public part, which contains some basic information. The second part is
+//! encrypted and basically contains the keys used to encrypt/decrypt the data
+//! of the container.
+//!
+//! ```rust
+//! // The following example creates a container.
+//!
+//! use nuts::container::Container;
+//! use nuts::types::Options;
+//! use tempfile::tempdir;
+//!
+//! // We put the container (named `container`) in a temporary directory.
+//! let dir = tempdir().unwrap();
+//! let file = dir.path().join("container");
+//!
+//! // The container is closed after creating the `Container` instance.
+//! // You need to create it explicity.
+//! let mut container = Container::new();
+//! assert!(!container.is_open());
+//!
+//! // You need some options to create the container. Here we take some
+//! // defaults for simplicity.
+//! let options = Options::default().unwrap();
+//!
+//! // Before creating the container using the `create()` method, you need to
+//! // assign a password callback. The returned password encrypts the secret
+//! // part of the header, which basically contains the keys used to encrypt
+//! // the data in the container.
+//! // Afterwards the container is open.
+//! container.set_password_callback(|| Ok(vec![1, 2, 3]));
+//! container.create(&file, &options).unwrap();
+//! assert!(container.is_open());
+//! ```
+//!
+//! ```rust
+//! // The following example opens an already existing container.
+//!
+//! use nuts::container::Container;
+//! use nuts::types::Options;
+//! use std::path::Path;
+//! use tempfile::tempdir;
+//!
+//! // The following function creates a container located in `file` with
+//! // default options.
+//! fn create_container(file: &Path) {
+//!   let mut container = Container::new();
+//!   let options = Options::default().unwrap();
+//!
+//!   container.set_password_callback(|| Ok(vec![1, 2, 3]));
+//!   container.create(&file, &options).unwrap();
+//! }
+//!
+//! // We put the container (named `container`) in a temporary directory.
+//! let dir = tempdir().unwrap();
+//! let file = dir.path().join("container");
+//!
+//! // Create the container.
+//! create_container(&file);
+//!
+//! // The container is closed after creating the `Container` instance.
+//! // You need to open it explicity.
+//! let mut container = Container::new();
+//! assert!(!container.is_open());
+//!
+//! // Before opening the container using the `open()` method, you need to
+//! // assign a password callback. The returned password encrypts the secret
+//! // part of the header, which basically contains the keys used to encrypt
+//! // the data in the container.
+//! // Afterwards the container is open.
+//! container.set_password_callback(|| Ok(vec![1, 2, 3]));
+//! container.open(&file, None).unwrap();
+//! assert!(container.is_open());
+//! ```
+//!
+//! ```rust
+//! // The following example writes data into the container and reads them
+//! // back.
+//!
+//! use nuts::container::Container;
+//! use nuts::types::Options;
+//! use std::path::Path;
+//! use tempfile::tempdir;
+//!
+//! // We put the container (named `container`) in a temporary directory.
+//! let dir = tempdir().unwrap();
+//! let file = dir.path().join("container");
+//!
+//! // For the example we create a container with some default options.
+//!
+//! let mut container = Container::new();
+//! let options = Options::default().unwrap();
+//!
+//! container.set_password_callback(|| Ok(vec![1, 2, 3]));
+//! container.create(&file, &options).unwrap();
+//!
+//! // Write 3 bytes into the block 1.
+//! let nbytes = container.write(1, b"123").unwrap();
+//! assert_eq!(nbytes, 3);
+//!
+//! // Read block #1 back.
+//! let mut buf = [0; 3];
+//! let nbytes = container.read(1, &mut buf).unwrap();
+//! assert_eq!(nbytes, 3);
+//! assert_eq!(&buf, b"123");
+//!
+//! ```
+
 #[macro_use]
 pub(crate) mod macros;
 
