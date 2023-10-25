4f74ced57a285982c92368d5ccb3c262caf43a69
Robin Doer <robin.doer@fast-lta.de>
2023-10-25 11:38:16 +0200

    Commit some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,183 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+//! A storage application inspired by the `tar` tool.
+//!
+//! The archive is an application based on the [nuts container]. Inspired by
+//! the `tar` tool you can store files, directories and symlinks in a
+//! [nuts container].
+//!
+//! * Entries can be appended at the end of the archive.
+//! * They cannot be removed from the archive.
+//! * You can travere the archive from the first to the last entry in the
+//!   archive.
+//!
+//! ## Create a new archive
+//!
+//! ```rust
+//! use nuts_archive::Archive;
+//! use nuts_container::container::{Cipher, Container, CreateOptionsBuilder};
+//! use nuts_directory::{CreateOptions, DirectoryBackend};
+//! use tempdir::TempDir;
+//!
+//! // Let's create a container (with a directory backend) in a temporary directory
+//! let tmp_dir = TempDir::new("nuts-archive").unwrap();
+//! let backend_options = CreateOptions::for_path(tmp_dir);
+//! let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+//!     .with_password_callback(|| Ok(b"123".to_vec()))
+//!     .build::<DirectoryBackend<TempDir>>()
+//!     .unwrap();
+//! let container =
+//!     Container::<DirectoryBackend<TempDir>>::create(backend_options, contaner_options).unwrap();
+//!
+//! // Now create an archive inside the container
+//! let archive = Archive::create(container, false).unwrap();
+//!
+//! // Fetch some information
+//! let info = archive.info();
+//! assert_eq!(info.blocks, 0);
+//! assert_eq!(info.files, 0);
+//! ```
+//!
+//! ## Open an existing archive
+//! ```rust
+//! use nuts_archive::Archive;
+//! use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+//! use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+//! use tempdir::TempDir;
+//!
+//! // This will create an empty archive in a temporary directory.
+//! let tmp_dir = {
+//!     let dir = TempDir::new("nuts-archive").unwrap();
+//!
+//!     let backend_options = CreateOptions::for_path(&dir);
+//!     let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+//!         .with_password_callback(|| Ok(b"123".to_vec()))
+//!         .build::<DirectoryBackend<&TempDir>>()
+//!         .unwrap();
+//!
+//!     let container =
+//!         Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+//!             .unwrap();
+//!     Archive::create(container, false).unwrap();
+//!
+//!     dir
+//! };
+//!
+//! // Open the container (with a directory backend) from the temporary directory.
+//! let backend_options = OpenOptions::for_path(tmp_dir);
+//! let container_options = OpenOptionsBuilder::new()
+//!     .with_password_callback(|| Ok(b"123".to_vec()))
+//!     .build::<DirectoryBackend<TempDir>>()
+//!     .unwrap();
+//! let container =
+//!     Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+//!
+//! // Open the archive
+//! let archive = Archive::open(container).unwrap();
+//!
+//! // Ferch some information
+//! let info = archive.info();
+//! assert_eq!(info.blocks, 0);
+//! assert_eq!(info.files, 0);
+//! ```
+//!
+//! ## Append an entry at the end of the archive
+//!
+//! ```rust
+//! use nuts_archive::Archive;
+//! use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+//! use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+//! use tempdir::TempDir;
+//!
+//! // This will create an empty archive in a temporary directory.
+//! let tmp_dir = {
+//!     let dir = TempDir::new("nuts-archive").unwrap();
+//!
+//!     let backend_options = CreateOptions::for_path(&dir);
+//!     let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+//!         .with_password_callback(|| Ok(b"123".to_vec()))
+//!         .build::<DirectoryBackend<&TempDir>>()
+//!         .unwrap();
+//!
+//!     let container =
+//!         Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+//!             .unwrap();
+//!     Archive::create(container, false).unwrap();
+//!
+//!     dir
+//! };
+//!
+//! // Open the container (with a directory backend) from the temporary directory.
+//! let backend_options = OpenOptions::for_path(tmp_dir);
+//! let container_options = OpenOptionsBuilder::new()
+//!     .with_password_callback(|| Ok(b"123".to_vec()))
+//!     .build::<DirectoryBackend<TempDir>>()
+//!     .unwrap();
+//! let container =
+//!     Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+//!
+//! // Open the archive
+//! let mut archive = Archive::open(container).unwrap();
+//!
+//! // Append a new entry
+//! let mut entry = archive.append("sample").build().unwrap();
+//! entry.write_all("some sample data".as_bytes()).unwrap();
+//! ```
+//!
+//! ## Loop through all entries in the archive
+//!
+//! ```rust
+//! use nuts_archive::Archive;
+//! use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+//! use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+//! use tempdir::TempDir;
+//!
+//! // This will create an empty archive in a temporary directory.
+//! let tmp_dir = {
+//!     let dir = TempDir::new("nuts-archive").unwrap();
+//!
+//!     let backend_options = CreateOptions::for_path(&dir);
+//!     let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+//!         .with_password_callback(|| Ok(b"123".to_vec()))
+//!         .build::<DirectoryBackend<&TempDir>>()
+//!         .unwrap();
+//!
+//!     let container =
+//!         Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+//!             .unwrap();
+//!     Archive::create(container, false).unwrap();
+//!
+//!     dir
+//! };
+//!
+//! // Open the container (with a directory backend) from the temporary directory.
+//! let backend_options = OpenOptions::for_path(tmp_dir);
+//! let container_options = OpenOptionsBuilder::new()
+//!     .with_password_callback(|| Ok(b"123".to_vec()))
+//!     .build::<DirectoryBackend<TempDir>>()
+//!     .unwrap();
+//! let container =
+//!     Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+//!
+//! // Open the archive and append two entries
+//! let mut archive = Archive::open(container).unwrap();
+//!
+//! archive.append("f1").build().unwrap();
+//! archive.append("f2").build().unwrap();
+//!
+//! // Go through the archive
+//! let entry = archive.first().unwrap().unwrap();
+//! assert_eq!(entry.name(), "f1");
+//!
+//! let entry = entry.next().unwrap().unwrap();
+//! assert_eq!(entry.name(), "f2");
+//!
+//! assert!(entry.next().is_none());
+//! ```
+//!
+//! [nuts container]: nuts_container
+
 mod container;
 mod entry;
 mod error;
@@ -50,6 +227,7 @@ pub struct Info {
     pub files: u64,
 }
 
+/// The archive.
 pub struct Archive<B: Backend> {
     container: BufContainer<B>,
     tree_id: B::Id,
---
+++ tests/archive.rs
@@ -0,0 +1,182 @@
+// MIT License
+//
+// Copyright (c) 2023 Robin Doer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#[test]
+fn create() {
+    use nuts_archive::Archive;
+    use nuts_container::container::{Cipher, Container, CreateOptionsBuilder};
+    use nuts_directory::{CreateOptions, DirectoryBackend};
+    use tempdir::TempDir;
+
+    // Let's create a container (with a directory backend) in a temporary directory
+    let tmp_dir = TempDir::new("nuts-archive").unwrap();
+    let backend_options = CreateOptions::for_path(tmp_dir);
+    let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+        .with_password_callback(|| Ok(b"123".to_vec()))
+        .build::<DirectoryBackend<TempDir>>()
+        .unwrap();
+    let container =
+        Container::<DirectoryBackend<TempDir>>::create(backend_options, contaner_options).unwrap();
+
+    // Now create an archive inside the container
+    let archive = Archive::create(container, false).unwrap();
+
+    // Fetch some information
+    let info = archive.info();
+    assert_eq!(info.blocks, 0);
+    assert_eq!(info.files, 0);
+}
+
+#[test]
+fn open() {
+    use nuts_archive::Archive;
+    use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+    use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+    use tempdir::TempDir;
+
+    // This will create an empty archive in a temporary directory.
+    let tmp_dir = {
+        let dir = TempDir::new("nuts-archive").unwrap();
+
+        let backend_options = CreateOptions::for_path(&dir);
+        let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+            .with_password_callback(|| Ok(b"123".to_vec()))
+            .build::<DirectoryBackend<&TempDir>>()
+            .unwrap();
+
+        let container =
+            Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+                .unwrap();
+        Archive::create(container, false).unwrap();
+
+        dir
+    };
+
+    // Open the container (with a directory backend) from the temporary directory.
+    let backend_options = OpenOptions::for_path(tmp_dir);
+    let container_options = OpenOptionsBuilder::new()
+        .with_password_callback(|| Ok(b"123".to_vec()))
+        .build::<DirectoryBackend<TempDir>>()
+        .unwrap();
+    let container =
+        Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+
+    // Open the archive
+    let archive = Archive::open(container).unwrap();
+
+    // Ferch some information
+    let info = archive.info();
+    assert_eq!(info.blocks, 0);
+    assert_eq!(info.files, 0);
+}
+
+#[test]
+fn append() {
+    use nuts_archive::Archive;
+    use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+    use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+    use tempdir::TempDir;
+
+    // This will create an empty archive in a temporary directory.
+    let tmp_dir = {
+        let dir = TempDir::new("nuts-archive").unwrap();
+
+        let backend_options = CreateOptions::for_path(&dir);
+        let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+            .with_password_callback(|| Ok(b"123".to_vec()))
+            .build::<DirectoryBackend<&TempDir>>()
+            .unwrap();
+
+        let container =
+            Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+                .unwrap();
+        Archive::create(container, false).unwrap();
+
+        dir
+    };
+
+    // Open the container (with a directory backend) from the temporary directory.
+    let backend_options = OpenOptions::for_path(tmp_dir);
+    let container_options = OpenOptionsBuilder::new()
+        .with_password_callback(|| Ok(b"123".to_vec()))
+        .build::<DirectoryBackend<TempDir>>()
+        .unwrap();
+    let container =
+        Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+
+    // Open the archive
+    let mut archive = Archive::open(container).unwrap();
+
+    // Append a new entry
+    let mut entry = archive.append("sample").build().unwrap();
+    entry.write_all("some sample data".as_bytes()).unwrap();
+}
+
+#[test]
+fn scan() {
+    use nuts_archive::Archive;
+    use nuts_container::container::{Cipher, Container, CreateOptionsBuilder, OpenOptionsBuilder};
+    use nuts_directory::{CreateOptions, DirectoryBackend, OpenOptions};
+    use tempdir::TempDir;
+
+    // This will create an empty archive in a temporary directory.
+    let tmp_dir = {
+        let dir = TempDir::new("nuts-archive").unwrap();
+
+        let backend_options = CreateOptions::for_path(&dir);
+        let contaner_options = CreateOptionsBuilder::new(Cipher::Aes128Gcm)
+            .with_password_callback(|| Ok(b"123".to_vec()))
+            .build::<DirectoryBackend<&TempDir>>()
+            .unwrap();
+
+        let container =
+            Container::<DirectoryBackend<&TempDir>>::create(backend_options, contaner_options)
+                .unwrap();
+        Archive::create(container, false).unwrap();
+
+        dir
+    };
+
+    // Open the container (with a directory backend) from the temporary directory.
+    let backend_options = OpenOptions::for_path(tmp_dir);
+    let container_options = OpenOptionsBuilder::new()
+        .with_password_callback(|| Ok(b"123".to_vec()))
+        .build::<DirectoryBackend<TempDir>>()
+        .unwrap();
+    let container =
+        Container::<DirectoryBackend<TempDir>>::open(backend_options, container_options).unwrap();
+
+    // Open the archive and append two entries
+    let mut archive = Archive::open(container).unwrap();
+
+    archive.append("f1").build().unwrap();
+    archive.append("f2").build().unwrap();
+
+    // Go through the archive
+    let entry = archive.first().unwrap().unwrap();
+    assert_eq!(entry.name(), "f1");
+
+    let entry = entry.next().unwrap().unwrap();
+    assert_eq!(entry.name(), "f2");
+
+    assert!(entry.next().is_none());
+}
