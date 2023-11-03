d949ad447c28a164998388e0dc7048520453fddc
Robin Doer <robin.doer@fast-lta.de>
2023-11-03 10:25:19 +0100

    --long option for `archive list`

--- src/cli/archive/list.rs
+++ src/cli/archive/list.rs
@@ -21,13 +21,82 @@
 // IN THE SOFTWARE.
 
 use anyhow::Result;
-use clap::Args;
-use nuts_archive::Archive;
+use clap::{ArgAction, Args};
+use log::debug;
+use nuts_archive::{Archive, Entry, Mode};
+use nuts_directory::DirectoryBackend;
+use std::ops::Deref;
+use std::path::PathBuf;
 
 use crate::cli::open_container;
 
+struct ListEntry {
+    name: String,
+    size: u64,
+    mode: Mode,
+}
+
+impl<'a> From<&'a Entry<'a, DirectoryBackend<PathBuf>>> for ListEntry {
+    fn from(entry: &Entry<DirectoryBackend<PathBuf>>) -> Self {
+        ListEntry {
+            name: entry.name().to_string(),
+            size: entry.size(),
+            mode: *entry.deref(),
+        }
+    }
+}
+
+fn collect_entries<'a>(
+    archive: &'a mut Archive<DirectoryBackend<PathBuf>>,
+) -> Result<Vec<ListEntry>> {
+    let mut vec = vec![];
+    let mut entry_opt = archive.first();
+
+    loop {
+        match entry_opt {
+            Some(Ok(entry)) => {
+                vec.push((&entry).into());
+                entry_opt = entry.next();
+            }
+            Some(Err(err)) => return Err(err.into()),
+            None => break,
+        }
+    }
+
+    Ok(vec)
+}
+
+fn print_short(entries: Vec<ListEntry>) {
+    for entry in entries {
+        println!("{}", entry.name);
+    }
+}
+
+fn print_long(entries: Vec<ListEntry>) {
+    let max_size = entries.iter().max_by_key(|e| e.size).map_or(0, |e| e.size);
+    let max_n = max_size.checked_ilog10().unwrap_or(0) as usize + 1;
+
+    for entry in entries {
+        let type_char = if entry.mode.is_file() {
+            '-'
+        } else if entry.mode.is_directory() {
+            'd'
+        } else if entry.mode.is_symlink() {
+            'l'
+        } else {
+            '?'
+        };
+
+        println!("{} {:>max_n$} {}", type_char, entry.size, entry.name);
+    }
+}
+
 #[derive(Args, Debug)]
 pub struct ArchiveListArgs {
+    /// Lists the content in the long format
+    #[clap(short, long, action = ArgAction::SetTrue)]
+    long: bool,
+
     /// Specifies the name of the container
     #[clap(short, long, env = "NUTS_CONTAINER")]
     container: String,
@@ -35,30 +104,18 @@ pub struct ArchiveListArgs {
 
 impl ArchiveListArgs {
     pub fn run(&self) -> Result<()> {
+        debug!("container: {}", self.container);
+        debug!("long: {}", self.long);
+
         let container = open_container(&self.container)?;
         let mut archive = Archive::open(container)?;
-        let mut entry_opt = archive.first();
 
-        loop {
-            match entry_opt {
-                Some(Ok(entry)) => {
-                    let type_char = if entry.is_file() {
-                        '-'
-                    } else if entry.is_directory() {
-                        'd'
-                    } else if entry.is_symlink() {
-                        'l'
-                    } else {
-                        '?'
-                    };
+        let entries = collect_entries(&mut archive)?;
 
-                    println!("{} {} ({})", type_char, entry.name(), entry.size());
-
-                    entry_opt = entry.next();
-                }
-                Some(Err(err)) => return Err(err.into()),
-                None => break,
-            }
+        if self.long {
+            print_long(entries);
+        } else {
+            print_short(entries);
         }
 
         Ok(())
