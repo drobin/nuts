6cc3216a494b996023a0f077d94a6c390966ca77
Robin Doer <robin.doer@fast-lta.de>
2024-06-18 10:55:23 +0200

    Put `archive add` subcommands into separate modules

--- nuts-tool/src/cli/archive/add/file.rs
+++ nuts-tool/src/cli/archive/add/file.rs
@@ -20,34 +20,18 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-pub mod dir;
-pub mod file;
-pub mod symlink;
-
 use anyhow::Result;
-use clap::{Args, Subcommand};
+use clap::Args;
 use log::debug;
 use nuts_archive::Archive;
 use std::io::{self, Read};
-use std::path::PathBuf;
 
-use crate::archive::append_recursive;
-use crate::cli::archive::add::dir::ArchiveAddDirectoryArgs;
-use crate::cli::archive::add::file::ArchiveAddFileArgs;
-use crate::cli::archive::add::symlink::ArchiveAddSymlinkArgs;
 use crate::cli::open_container;
 
 #[derive(Args, Debug)]
-// #[clap(group(ArgGroup::new("input").required(true).multiple(false)))]
-#[clap(args_conflicts_with_subcommands = true)]
-pub struct ArchiveAddArgs {
-    #[clap(subcommand)]
-    command: Option<ArchiveAddCommand>,
-
-    /// Path to files/directories to be added to the archive. If PATHS contains
-    /// a directory all entries in the directory are also appended. If no PATHS
-    /// are specified an empty archive is created.
-    paths: Vec<PathBuf>,
+pub struct ArchiveAddFileArgs {
+    /// Name of the file.
+    name: String,
 
     /// Specifies the name of the container
     #[clap(short, long, env = "NUTS_CONTAINER")]
@@ -57,43 +41,28 @@ pub struct ArchiveAddArgs {
     verbose: u8,
 }
 
-impl ArchiveAddArgs {
+impl ArchiveAddFileArgs {
     pub fn run(&self) -> Result<()> {
-        if let Some(command) = self.command.as_ref() {
-            return command.run();
-        }
-
         debug!("args: {:?}", self);
 
         let container = open_container(&self.container, self.verbose)?;
         let mut archive = Archive::open(container)?;
 
-        for path in self.paths.iter() {
-            append_recursive(&mut archive, path)?;
+        let block_size = archive.as_ref().block_size() as usize;
+        let mut entry = archive.append_file(&self.name).build()?;
+        let mut buf = vec![0; block_size];
+
+        loop {
+            let n = io::stdin().read(&mut buf)?;
+            debug!("{} bytes read from stdin", n);
+
+            if n > 0 {
+                entry.write_all(&buf[..n])?;
+            } else {
+                break;
+            }
         }
 
         Ok(())
-    }
-}
-
-#[derive(Debug, Subcommand)]
-pub enum ArchiveAddCommand {
-    /// Appends a custom file to the archive.
-    File(ArchiveAddFileArgs),
-
-    /// Appends a custom directory to the archive.
-    Directory(ArchiveAddDirectoryArgs),
-
-    /// Appends a custom symlink to the archive.
-    Symlink(ArchiveAddSymlinkArgs),
-}
-
-impl ArchiveAddCommand {
-    pub fn run(&self) -> Result<()> {
-        match self {
-            Self::File(args) => args.run(),
-            Self::Directory(args) => args.run(),
-            Self::Symlink(args) => args.run(),
-        }
     }
 }
