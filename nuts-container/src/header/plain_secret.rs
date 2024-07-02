8b87e42f5a71f338d3622ab9126b151327669d95
Robin Doer <robin.doer@fast-lta.de>
2024-07-02 09:45:54 +0200

    Make a revision 1 `PlainSecret`

--- nuts-container/src/header/plain_secret.rs
+++ nuts-container/src/header/plain_secret.rs
@@ -35,8 +35,43 @@ use crate::ossl;
 use crate::password::PasswordStore;
 use crate::svec::SecureVec;
 
-fn generate_magics() -> Result<[u32; 2], ErrorStack> {
-    ossl::rand_u32().map(|magic| [magic, magic])
+// ** plain-secret history **
+//
+// * rev 0
+//
+// inital version
+//
+// * rev 1
+//
+// - userdata field removed
+// - top_id field inserted
+// - decrease vec lengths from 8 to 1, settings from 8 to 2
+
+#[derive(Debug, PartialEq)]
+struct Magics([u32; 2]);
+
+impl Magics {
+    fn generate() -> Result<Magics, ErrorStack> {
+        ossl::rand_u32().map(|magic| Magics([magic, magic]))
+    }
+
+    fn get_and_validate<T: Buffer>(buf: &mut T) -> Result<Magics, HeaderError> {
+        let n1 = buf.get_u32()?;
+        let n2 = buf.get_u32()?;
+
+        if n1 == n2 {
+            Ok(Magics([n1, n2]))
+        } else {
+            Err(HeaderError::WrongPassword)
+        }
+    }
+
+    fn put<T: BufferMut>(&self, buf: &mut T) -> Result<(), BufferError> {
+        buf.put_u32(self.0[0])?;
+        buf.put_u32(self.0[1])?;
+
+        Ok(())
+    }
 }
 
 pub trait Encryptor: ToBuffer + Sized {
@@ -68,7 +103,7 @@ pub trait Encryptor: ToBuffer + Sized {
 
 #[derive(Debug, PartialEq)]
 pub struct PlainSecretRev0 {
-    magics: [u32; 2],
+    magics: Magics,
     pub key: SecureVec,
     pub iv: SecureVec,
     pub userdata: SecureVec,
@@ -83,7 +118,7 @@ impl PlainSecretRev0 {
         settings: SecureVec,
     ) -> Result<PlainSecretRev0, ErrorStack> {
         Ok(PlainSecretRev0 {
-            magics: generate_magics()?,
+            magics: Magics::generate()?,
             key,
             iv,
             userdata,
@@ -96,20 +131,14 @@ impl FromBuffer for PlainSecretRev0 {
     type Error = HeaderError;
 
     fn from_buffer<T: Buffer>(buf: &mut T) -> Result<Self, HeaderError> {
-        let magic1 = buf.get_u32()?;
-        let magic2 = buf.get_u32()?;
-
-        if magic1 != magic2 {
-            return Err(HeaderError::WrongPassword);
-        }
-
+        let magics = Magics::get_and_validate(buf)?;
         let key = buf.get_vec::<8>()?.into();
         let iv = buf.get_vec::<8>()?.into();
         let userdata = buf.get_vec::<8>()?.into();
         let settings = buf.get_vec::<8>()?.into();
 
         Ok(PlainSecretRev0 {
-            magics: [magic1, magic2],
+            magics,
             key,
             iv,
             userdata,
@@ -120,8 +149,7 @@ impl FromBuffer for PlainSecretRev0 {
 
 impl ToBuffer for PlainSecretRev0 {
     fn to_buffer<T: BufferMut>(&self, buf: &mut T) -> Result<(), BufferError> {
-        buf.put_u32(self.magics[0])?;
-        buf.put_u32(self.magics[1])?;
+        self.magics.put(buf)?;
         buf.put_vec::<8>(&self.key)?;
         buf.put_vec::<8>(&self.iv)?;
         buf.put_vec::<8>(&self.userdata)?;
@@ -132,3 +160,70 @@ impl ToBuffer for PlainSecretRev0 {
 }
 
 impl Encryptor for PlainSecretRev0 {}
+
+#[derive(Debug, PartialEq)]
+pub struct PlainSecretRev1 {
+    magics: Magics,
+    pub key: SecureVec,
+    pub iv: SecureVec,
+    pub top_id: Option<SecureVec>,
+    pub settings: SecureVec,
+}
+
+impl PlainSecretRev1 {
+    pub fn generate(
+        key: SecureVec,
+        iv: SecureVec,
+        top_id: Option<SecureVec>,
+        settings: SecureVec,
+    ) -> Result<PlainSecretRev1, ErrorStack> {
+        Ok(PlainSecretRev1 {
+            magics: Magics::generate()?,
+            key,
+            iv,
+            top_id,
+            settings,
+        })
+    }
+}
+
+impl FromBuffer for PlainSecretRev1 {
+    type Error = HeaderError;
+
+    fn from_buffer<T: Buffer>(buf: &mut T) -> Result<Self, HeaderError> {
+        let magics = Magics::get_and_validate(buf)?;
+        let key = buf.get_vec::<1>()?.into();
+        let iv = buf.get_vec::<1>()?.into();
+        let top_id = buf.get_vec::<1>()?;
+        let settings = buf.get_vec::<2>()?.into();
+
+        let top_id = (!top_id.is_empty()).then(|| top_id.into());
+
+        Ok(PlainSecretRev1 {
+            magics,
+            key,
+            iv,
+            top_id,
+            settings,
+        })
+    }
+}
+
+impl ToBuffer for PlainSecretRev1 {
+    fn to_buffer<T: BufferMut>(&self, buf: &mut T) -> Result<(), BufferError> {
+        self.magics.put(buf)?;
+        buf.put_vec::<1>(&self.key)?;
+        buf.put_vec::<1>(&self.iv)?;
+
+        match self.top_id.as_ref() {
+            Some(vec) => buf.put_vec::<1>(vec)?,
+            None => buf.put_vec::<1>(&[])?,
+        }
+
+        buf.put_vec::<2>(&self.settings)?;
+
+        Ok(())
+    }
+}
+
+impl Encryptor for PlainSecretRev1 {}
--- nuts-container/src/header/plain_secret/tests.rs
+++ nuts-container/src/header/plain_secret/tests.rs
@@ -20,4 +20,195 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-mod rev0;
+macro_rules! make_tests {
+    ($ty:ty) => {
+        use std::rc::Rc;
+
+        use crate::buffer::{FromBuffer, ToBuffer};
+        use crate::cipher::Cipher;
+        use crate::digest::Digest;
+        use crate::header::plain_secret::Encryptor;
+        use crate::header::HeaderError;
+        use crate::kdf::Kdf;
+        use crate::password::PasswordStore;
+
+        #[test]
+        fn ser() {
+            let mut buf = vec![];
+
+            plain_secret().to_buffer(&mut buf).unwrap();
+            assert_eq!(buf, PLAIN_SECRET);
+        }
+
+        #[test]
+        fn de() {
+            let out = <$ty>::from_buffer(&mut &PLAIN_SECRET[..]).unwrap();
+
+            assert_eq!(out, plain_secret());
+        }
+
+        #[test]
+        fn de_inval() {
+            let mut vec = PLAIN_SECRET.to_vec();
+            vec[0] += 1;
+
+            let err = <$ty>::from_buffer(&mut vec.as_slice()).unwrap_err();
+            assert!(matches!(err, HeaderError::WrongPassword));
+        }
+
+        #[test]
+        fn encrypt_none() {
+            let cb = || panic!("callback should never be called");
+            let mut store = PasswordStore::new(Some(Rc::new(cb)));
+
+            let secret = plain_secret()
+                .encrypt(&mut store, Cipher::None, &Kdf::None, &[])
+                .unwrap();
+            assert_eq!(secret, PLAIN_SECRET);
+        }
+
+        #[test]
+        fn encrypt_some() {
+            let cb = || Ok(vec![1, 2, 3]);
+            let mut store = PasswordStore::new(Some(Rc::new(cb)));
+
+            let kdf = Kdf::pbkdf2(Digest::Sha1, 1, &[0]);
+            let secret = plain_secret()
+                .encrypt(&mut store, Cipher::Aes128Ctr, &kdf, &[1; 16])
+                .unwrap();
+            assert_eq!(secret, SECRET);
+        }
+    };
+}
+
+mod rev0 {
+    use crate::header::plain_secret::{Magics, PlainSecretRev0};
+
+    // key: AE 18 FF 41 77 79 0F 07 AB 11 E2 F1 8C 87 AD 9A
+    // iv: 01010101010101010101010101010101
+    const SECRET: [u8; 45] = [
+        0x5c, 0x68, 0x30, 0x8f, 0x47, 0x19, 0xf4, 0x76, 0xf2, 0x72, 0xbc, 0x6, 0x1c, 0xf3, 0x58,
+        0xca, 0x54, 0x2c, 0xca, 0xf8, 0xe6, 0x7d, 0xe1, 0xfb, 0xb4, 0xe1, 0x1c, 0xbe, 0xb7, 0x83,
+        0x54, 0x3b, 0xec, 0x8c, 0xee, 0xac, 0x59, 0x21, 0x58, 0xb3, 0x71, 0x90, 0x41, 0x4c, 0xe4,
+    ];
+
+    const PLAIN_SECRET: [u8; 45] = [
+        0x00, 0x00, 0x12, 0x67, // magic1
+        0x00, 0x00, 0x12, 0x67, // magic2
+        0, 0, 0, 0, 0, 0, 0, 2, 1, 2, // key
+        0, 0, 0, 0, 0, 0, 0, 3, 3, 4, 5, // iv
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Some(top-id)
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // settings (empty)
+    ];
+
+    fn plain_secret() -> PlainSecretRev0 {
+        PlainSecretRev0 {
+            magics: Magics([4711, 4711]),
+            key: vec![1, 2].into(),
+            iv: vec![3, 4, 5].into(),
+            userdata: vec![].into(),
+            settings: vec![].into(),
+        }
+    }
+
+    #[test]
+    fn generate() {
+        let plain_secret = PlainSecretRev0::generate(
+            vec![1].into(),
+            vec![2, 3].into(),
+            vec![4, 5, 6].into(),
+            vec![7, 8, 9, 10].into(),
+        )
+        .unwrap();
+
+        assert_eq!(plain_secret.magics.0[0], 0x91C0B2CF);
+        assert_eq!(plain_secret.magics.0[1], 0x91C0B2CF);
+        assert_eq!(*plain_secret.key, [1]);
+        assert_eq!(*plain_secret.iv, [2, 3]);
+        assert_eq!(*plain_secret.userdata, [4, 5, 6]);
+        assert_eq!(*plain_secret.settings, [7, 8, 9, 10]);
+    }
+
+    make_tests!(PlainSecretRev0);
+}
+
+mod rev1 {
+    use crate::header::plain_secret::{Magics, PlainSecretRev1};
+
+    // key: AE 18 FF 41 77 79 0F 07 AB 11 E2 F1 8C 87 AD 9A
+    // iv: 01010101010101010101010101010101
+    const SECRET: [u8; 27] = [
+        0x5c, 0x68, 0x30, 0x8f, 0x47, 0x19, 0xf4, 0x76, 0xf0, 0x73, 0xbe, 0x05, 0x1f, 0xf7, 0x5d,
+        0xcc, 0x53, 0x29, 0xc2, 0xf1, 0xe6, 0x78, 0xeb, 0xf0, 0xb8, 0xef, 0x11,
+    ];
+
+    const PLAIN_SECRET: [u8; 27] = [
+        0x00, 0x00, 0x12, 0x67, // magic1
+        0x00, 0x00, 0x12, 0x67, // magic2
+        2, 1, 2, // key
+        3, 3, 4, 5, // iv
+        4, 6, 7, 8, 9, // top-id
+        0, 5, 10, 11, 12, 13, 14, // settings
+    ];
+
+    const PLAIN_SECRET_NO_TOP_ID: [u8; 23] = [
+        0x00, 0x00, 0x12, 0x67, // magic1
+        0x00, 0x00, 0x12, 0x67, // magic2
+        2, 1, 2, // key
+        3, 3, 4, 5, // iv
+        0, // top-id
+        0, 5, 10, 11, 12, 13, 14, // settings
+    ];
+
+    fn plain_secret() -> PlainSecretRev1 {
+        PlainSecretRev1 {
+            magics: Magics([4711, 4711]),
+            key: vec![1, 2].into(),
+            iv: vec![3, 4, 5].into(),
+            top_id: Some(vec![6, 7, 8, 9].into()),
+            settings: vec![10, 11, 12, 13, 14].into(),
+        }
+    }
+
+    fn plain_secret_no_top_id() -> PlainSecretRev1 {
+        PlainSecretRev1 {
+            top_id: None,
+            ..plain_secret()
+        }
+    }
+
+    #[test]
+    fn generate() {
+        let plain_secret = PlainSecretRev1::generate(
+            vec![1].into(),
+            vec![2, 3].into(),
+            Some(vec![4, 5, 6].into()),
+            vec![7, 8, 9, 10].into(),
+        )
+        .unwrap();
+
+        assert_eq!(plain_secret.magics.0[0], 0x91C0B2CF);
+        assert_eq!(plain_secret.magics.0[1], 0x91C0B2CF);
+        assert_eq!(*plain_secret.key, [1]);
+        assert_eq!(*plain_secret.iv, [2, 3]);
+        assert_eq!(*plain_secret.top_id.unwrap(), [4, 5, 6]);
+        assert_eq!(*plain_secret.settings, [7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn ser_no_top_id() {
+        let mut buf = vec![];
+
+        plain_secret_no_top_id().to_buffer(&mut buf).unwrap();
+        assert_eq!(buf, PLAIN_SECRET_NO_TOP_ID);
+    }
+
+    #[test]
+    fn de_no_top_id() {
+        let out = PlainSecretRev1::from_buffer(&mut &PLAIN_SECRET_NO_TOP_ID[..]).unwrap();
+
+        assert_eq!(out, plain_secret_no_top_id());
+    }
+
+    make_tests!(PlainSecretRev1);
+}
